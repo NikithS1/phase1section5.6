11cfae2ff9a1721ac183d7536ce59eb2ae8e518f
Nikiths1 <nikith.s1@mphasis.com>
2023-08-02 11:37:31 +0530

    first commit

---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1section5._6
+{
+    internal class Student
+    {
+    }
+}
